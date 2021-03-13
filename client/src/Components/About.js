e0cc5a1d6048b35930c4853b923f22f2a1564688
Ritul Deshmukh <ritulrdeshmukh@gmail.com>
2021-03-13 21:52:05 +0530

    Ketki

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -21,6 +21,18 @@ class About extends Component {
 
                     <Card>
                         <Card.Body>
+                        <Card.Title>Ketki Fadnavis</Card.Title>
+                        <Card.Text>
+                            I am currently pursuing Computer Science and Engineering from Shri Ramdeobaba College of Engineering and Management.
+                        </Card.Text>
+                        </Card.Body>
+                        <Card.Footer>
+                        <small className="text-muted">III Year B.Tech CSE</small>
+                        </Card.Footer>
+                    </Card>
+
+                    <Card>
+                        <Card.Body>
                         <Card.Title>Ritul Deshmukh</Card.Title>
                         <Card.Text>
                             I am currently pursuing Computer Science and Engineering from Shri Ramdeobaba College of Engineering and Management. My special interest lies in Agile Development and Project Management in Software Engineering.
